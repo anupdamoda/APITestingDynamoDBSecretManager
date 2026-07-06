0f76823b8af3bb44075f0c937675d8f45d00d957
Ace Automation <aceautomation.australia@gmail.com>
2026-07-07 08:05:17 +1000

    fixing the cucumber file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,6 @@ TEST_EXIT_CODE=$?
 
 RUN_ID=$(date +%Y%m%d-%H%M%S)
 
-aws s3 cp target/cucumber-html-reports s3://ace-automation-bucket-2026-ap-southeast-2/reports/RUN_ID/ --recursive
+aws s3 cp target/cucumber-report.html s3://ace-automation-bucket-2026-ap-southeast-2/reports/RUN_ID/ --recursive
 
 exit $TEST_EXIT_CODE
