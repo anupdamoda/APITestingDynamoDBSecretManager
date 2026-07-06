13b7bc4d1f18ab830edba204931070171070b063
Ace Automation <aceautomation.australia@gmail.com>
2026-07-07 07:58:16 +1000

    adding the s3 report

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,3 +11,11 @@ echo "Generating Allure Report..."
 ls -R target
 
 echo "Tests completed successfully"
+
+TEST_EXIT_CODE=$?
+
+RUN_ID=$(date +%Y%m%d-%H%M%S)
+
+aws s3 cp target/cucumber-html-reports s3://ace-automation-bucket-2026-ap-southeast-2/reports/RUN_ID/ --recursive
+
+exit $TEST_EXIT_CODE
