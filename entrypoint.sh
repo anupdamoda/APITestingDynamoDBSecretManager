6267512338481c2e76b37ab1cdc617bee4168619
Ace Automation <aceautomation.australia@gmail.com>
2026-07-07 08:12:32 +1000

    adding the echo statements for s3 location

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,14 @@ TEST_EXIT_CODE=$?
 
 RUN_ID=$(date +%Y%m%d-%H%M%S)
 
+echo "Uploading Allure Report to S3..."
+
+echo "RUN_ID: $RUN_ID"
+
 aws s3 cp target/cucumber-report.html s3://ace-automation-bucket-2026-ap-southeast-2/reports/RUN_ID/ --recursive
+
+echo "Allure Report uploaded successfully to S3"
+
+echo "s3://ace-automation-bucket-2026-ap-southeast-2/reports/${RUN_ID}/"
 
 exit $TEST_EXIT_CODE
